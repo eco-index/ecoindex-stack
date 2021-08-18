d822a062514c12cb560bfdf72c651ba967cb1425
Konny Brown <86269916+konnyb@users.noreply.github.com>
2021-08-18 17:34:12 +1200

    Initial commit again

---
+++ web/app/api/routes/__init__.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from app.api.routes.cleanings import router as cleanings_router
+
+router = APIRouter()
+
+router.include_router(cleanings_router, prefix="/cleanings", tags=["cleanings"])
